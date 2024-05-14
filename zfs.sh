a568d66046aa0466758dfeabbfea9c2c98f6a52a
root <evgenylisovik@gmail.com>
2024-05-15 00:18:49 +0300

    first commit

---
+++ zfs.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#install zfs repo
+yum install -y http://download.zfsonlinux.org/epel/zfs-release.el7_8.noarch.rpm
+#import gpg key 
+rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-zfsonlinux
+#install DKMS style packages for correct work ZFS
+yum install -y epel-release kernel-devel zfs
+#change ZFS repo
+yum-config-manager --disable zfs
+yum-config-manager --enable zfs-kmod
+yum install -y zfs
+#Add kernel module zfs
+modprobe zfs
+#install wget
+yum install -y wget
+zpool create otus1 mirror /dev/sdb /dev/sdc
+zpool create otus2 mirror /dev/sdd /dev/sde
+zpool create otus3 mirror /dev/sdf /dev/sdg
+zpool create otus4 mirror /dev/sdh /dev/sdi
+zfs set compression=lzjb otus1
+zfs set compression=lz4 otus2
+zfs set compression=gzip-9 otus3
+zfs set compression=zle otus4
+for i in {1..4}; do wget -P /otus$i https://gutenberg.org/cache/epub/2600/pg2600.converter.log; done
+wget -O archive.tar.gz --no-check-certificate 'https://drive.usercontent.google.com/download?id=1MvrcEp-WgAQe57aDEzxSRalPAwbNN1Bb&export=download'
+tar -xzvf archive.tar.gz
+zpool import -d zpoolexport/ otus
+wget -O otus_task2.file --no-check-certificate 'https://drive.usercontent.google.com/download?id=1wgxjih8YZ-cqLqaZVa0lA3h3Y029c3oI&export=download
+zfs receive otus/test@today < /home/vagrant/otus_task2.file'
